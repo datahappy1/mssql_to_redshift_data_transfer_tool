f8319af75d857d2261c9341fc66d634b0c1f0000
Pprud <Pavel.Prudky@Accolade.com>
2018-10-31 13:32:00 +0100

    added notes for env.variables

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='mssql_to_redshift',
+    name='mssql_to_redshift_data_transfer_tool',
     version='1.0',
     packages=[''],
-    url='',
+    url='https://github.com/datahappy1/mssql_to_redshift_data_transfer_tool/',
     license='MIT',
     author='datahappy1',
     author_email='',
-    description='',
+    description='Python 3.7 tool to extract data out of a MSSQL database and load it into an AWS Redshift Cluster',
     install_requires=['boto3', 'psycopg2', 'pymssql']
 )
