ff4f6074f6cb6c31bbe10426360112aa4dd13480
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2018-11-26 23:28:33 +0100

    test integr.  Please enter the commit message for your changes. Lines starting

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,28 +1,39 @@
+""" integration test """
+
 import os
 import logging
-from src import mssql_to_redshift
 from io import StringIO
+from src import mssql_to_redshift
 
 
-# assuming we can extract a test table to a csv file, upload to S3 and the Redshift copy command is as expected
+# assuming we can extract a test table to a csv file, upload to S3
+# the Redshift copy command is looking as expected and program returns success
 def test_integrate():
-
-    targetdir = os.getcwd().rstrip('tests') + 'files'
+    """
+    main test integration function
+    :return:
+    """
+    target_dir = os.getcwd().rstrip('tests') + 'files'
     log_stream = StringIO()
     logging.basicConfig(stream=log_stream, level=logging.INFO)
 
-    mssql_to_redshift.main('MSSQL_to_Redshift', 'mngmt', targetdir, 'TRUE')
+    obj = mssql_to_redshift.Runner(database_name='MSSQL_to_Redshift', schema_name='mngmt',
+                                   target_directory=target_dir, dry_run=1, dry_run_str_prefix='',
+                                   files='', ret='')
 
-    s3commandfound = 0
-    redshiftcommandfound = 0
+    mssql_to_redshift.Runner.main(obj)
+
+    success_found = 0
 
     for line in log_stream.getvalue().splitlines():
-        if 'Dryrun S3 copy Integration_test_table' in line:
-            s3commandfound = 1
-        elif 'Dryrun copy Integration_test_table Integration_test_table_' in line:
-            redshiftcommandfound = 1
+        print(line)
+        if 'Program ran successfully!' in line:
+            success_found = 1
         else:
             pass
-    print(s3commandfound)
-    assert  s3commandfound == 1
-    #assert s3commandfound + redshiftcommandfound == 2
+    #print(success_found)
+
+    assert success_found == 1
+
+
+#test_integrate()
