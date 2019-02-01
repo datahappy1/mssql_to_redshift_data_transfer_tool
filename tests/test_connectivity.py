e21db4f42728a9b635d5c55647a258c75ba48ce2
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2019-02-01 21:16:40 +0100

    Update test_connectivity.py

--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -31,17 +31,17 @@ def test_connect_redshift():
     assert test == "(1,)"
 
 
-# assuming we can initiate a connection to AWS S3 and checking
-# the bucket we setup in settings.py exists and that we can connect
-# if success, test_pass returns 0 used in assertion
+# assuming we can initiate a connection to AWS S3 and checking that
+# the bucket name we declare in settings.py exists and that we have access
+# if success, variable "test" returns 0 used for assertion
 def test_s3_init():
     """
     AWS S3 test connectivity function
     :return:
     """
-    test_pass = 1
+    test = 1
     try:
         conn_s3 = aws.init_s3()
-        test_pass = aws.check_bucket(conn_s3)
+        test = aws.check_bucket(conn_s3)
     finally:
-        assert test_pass == 0
+        assert test == 0
